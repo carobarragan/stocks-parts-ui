81e6a9a93b8fab62215156c4fb07af59fcec1d73
carobarragan <104798095+carobarragan@users.noreply.github.com>
2025-03-28 13:52:57 -0300

    borrado la imagen del logo - ahora se utiliza un icon * se instalo una dependencia para usar el icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,44 +1,33 @@
 <script lang="ts">
 	import '../app.postcss';
 	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
-
-	// Floating UI para Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
 	import '@fortawesome/fontawesome-free/css/all.min.css';
-
-	// Componente del modal
 	import FeedbackModal from '../lib/component/modal.svelte';
-
-	// Importar el store de búsqueda
 	import { searchTerm } from '../lib/store/searchStore';
+	import { Car } from 'phosphor-svelte';
 
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 
-	// Estado para el menú hamburguesa
 	let isMenuOpen: boolean = false;
-	// Estado para el modal de feedback
 	let isFeedbackModalOpen: boolean = false;
 
-	// Función para manejar el cambio en el input de búsqueda
 	function handleSearchInput(event: Event): void {
 		const target = event.target as HTMLInputElement;
-		$searchTerm = target.value; // Actualiza el store con el valor del input
+		$searchTerm = target.value;
 	}
 </script>
 
-<!-- App Shell -->
 <AppShell>
 	<svelte:fragment slot="header">
-		<!-- App Bar -->
 		<AppBar background="bg-gray-800" padding="p-4" shadow="shadow-md">
 			<svelte:fragment slot="lead">
 				<div class="flex items-center justify-between w-full md:w-auto">
-					<!-- Logo -->
-					<div class="flex items-center space-x-4">
-						<img src="src/lib/logo.png" alt="AutoParts Inventory" class="h-12 md:h-16" />
+					<div class="flex items-center space-x-2">
+						<Car size={40} class="text-white" weight="fill" />
+						<span class="text-xl md:text-2xl font-bold text-white">AutoParts</span>
 					</div>
-					<!-- Botón de menú hamburguesa (visible solo en pantallas pequeñas) -->
 					<button
 						class="md:hidden text-white"
 						on:click={() => (isMenuOpen = !isMenuOpen)}
@@ -68,13 +57,11 @@
 			</svelte:fragment>
 
 			<svelte:fragment slot="trail">
-				<!-- Contenedor para barra de búsqueda y botones -->
 				<div
 					class="flex flex-col md:flex-row items-center space-y-4 md:space-y-0 md:space-x-4 w-full {isMenuOpen
 						? 'flex'
 						: 'hidden md:flex'}"
 				>
-					<!-- Barra de búsqueda -->
 					<div class="w-full max-w-md relative flex items-center">
 						<i class="fa fa-search absolute left-3 text-gray-400"></i>
 						<input
@@ -85,10 +72,7 @@
 							bind:value={$searchTerm}
 						/>
 					</div>
-
-					<!-- Botones -->
 					<div class="flex space-x-2">
-						<!-- Botón de Feedback -->
 						<button
 							type="button"
 							class="btn variant-filled-primary flex items-center space-x-2"
@@ -97,8 +81,6 @@
 							<i class="fa fa-commenting" aria-hidden="true"></i>
 							<span>Feedback</span>
 						</button>
-
-						<!-- Botón de GitHub -->
 						<a
 							class="btn variant-filled-primary flex items-center space-x-2"
 							href="https://github.com/skeletonlabs/skeleton"
@@ -113,9 +95,6 @@
 		</AppBar>
 	</svelte:fragment>
 
-	<!-- Page Route Content -->
 	<slot />
-
-	<!-- Modal de Feedback -->
 	<FeedbackModal bind:isOpen={isFeedbackModalOpen} />
 </AppShell>
